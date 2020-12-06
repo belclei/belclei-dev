9e25af4402716739f9815644f8ff18bde52a6d48
belclei <belclei@gmail.com>
2020-12-06 19:28:36 -0300

    setup styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
+import GlobalStyle from '@/styles/GlobalStyle'
 import '../styles/reset.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
