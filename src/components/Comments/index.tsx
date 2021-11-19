a31b8708e5dad1e1eaad9d030b1b43e955a0f4d4
belclei <belclei@gmail.com>
2021-11-19 17:56:57 -0300

    enforce not nullable element

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -4,13 +4,14 @@ export default function Comments(): JSX.Element {
   useEffect(() => {
     const script = document.createElement('script')
     const anchor = document.getElementById('comments')
+
     script.setAttribute('src', 'https://utteranc.es/client.js')
     script.setAttribute('crossorigin', 'anonymous')
     script.setAttribute('async', 'true')
     script.setAttribute('repo', 'belclei/belclei-dev')
     script.setAttribute('issue-term', 'pathname')
     script.setAttribute('theme', 'dark-blue')
-    anchor.appendChild(script)
+    anchor!.appendChild(script)
   }, [])
 
   return <div id="comments" />
